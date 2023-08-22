bd1115112d17ecc58bb59061686293a0adbead40
zubied-asif <zubied.asif@gmail.com>
2023-08-22 23:25:30 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+function getInputValue(id){
+    const inputField = document.getElementById(id);
+    const value = parseFloat(inputField.value);
+    return value;
+} 
+function getInputText(id){
+    const inputField = document.getElementById(id);
+    const value = parseFloat(inputField.innerHTML);
+    return value;
+} 
+function funOne(){
+    const income = getInputValue('income');
+    const food = getInputValue('food');
+    const rent = getInputValue('rent');
+    const clothes = getInputValue('clothes');
+
+    const totalExpense = document.getElementById('total');
+    const remaining = document.getElementById('remaining');
+
+    const expense = food+rent+clothes;
+    const total = income - expense ;
+    
+
+    totalExpense.innerText = expense;
+    remaining.innerText = total 
+    
+}
+
+function funTwo(){
+
+    const income = getInputValue('income');
+    const food = getInputValue('food');
+    const rent = getInputValue('rent');
+    
+    const clothes = getInputValue('clothes');
+   
+    const expense = food+rent+clothes;
+ 
+    const total = income - expense ;
+   
+
+    
+    const save = getInputValue('saving');
+    
+    const savingAmount = document.getElementById('saving-amount');
+   
+    savingAmount.innerText = total*save / 100
+  
+    const remainingAmount = document.getElementById('remaining-amount');
+
+    remainingAmount.innerHTML = total - savingAmount.innerText 
+
+
+    
+}
